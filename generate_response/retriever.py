a7c6f21d1b4851bcc816ba3e2a985fde8f1d4e3c
Ella Pham <epham@deloitte.co.uk>
2024-03-30 13:09:25 +0000

    Refactor Generate Response function

--- generate_response/retriever.py
+++ generate_response/retriever.py
@@ -54,7 +54,7 @@ def generate_response(link, openai_api_key, query_text):
     template = """Use the following pieces of context to answer the question at the end.
     If you don't know the answer, just say that you don't know, don't try to make up an answer.
     Use three sentences maximum and keep the answer as concise as possible.
-    Always say "thanks for asking, Ella!" at the end of the answer. However, say it in a separate paragraph at the end. Sign off your answer by 'Razi Oppa'.
+    Always say "thanks for asking!" at the end of the answer. However, say it in a separate paragraph at the end. Sign off your answer by 'DocumentQA'.
 
     {context}
 
@@ -79,4 +79,4 @@ def generate_response(link, openai_api_key, query_text):
     return rag_chain.invoke(query_text)
 
 if __name__ == "__main__":
-    generate_response(link=link, openai_api_key=openai_api_key, query_text=query_text)
+    generate_response()
